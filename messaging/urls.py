9186b258815a0b849837a41af95d02b852f1f1ff
datagutten <datagutten@datagutten.net>
2019-07-29 12:17:13 +0200

    Send messages from HTTP POST request

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .views import send_message
+from .views import send_message, send_message_post
 
 app_name = 'messaging'
 
 urlpatterns = [
     path('send/<str:recipient>/<str:message>', send_message, name='send'),
+    path('send_post', send_message_post, name='send_post'),
 ]
--- messaging/views.py
+++ messaging/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import SendMessage
 
 
-def send_message(recipient, message):
+# TODO: Add authentication
+
+
+def send_message(request, recipient, message):
     msg = SendMessage(recipient=recipient, text=message)
+    msg.save()
+    return HttpResponse('Added to send queue')
+
+
+@csrf_exempt
+def send_message_post(request):
+    data = request.POST
+    msg = SendMessage(recipient=data['recipient'], text=data['message'])
     msg.save()
     return HttpResponse('Added to send queue')
